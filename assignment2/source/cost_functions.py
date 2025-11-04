ed0c6311f15fb4d561879fc15d0a3d6144ed8460
geomort <morten.taraldsten.brunes@kartverket.no>
2025-11-04 20:19:54 +0100

    added cost functions with regularization terms

--- assignment2/source/cost_functions.py
+++ assignment2/source/cost_functions.py
@@ -42,3 +42,73 @@ class cross_entropy(cost_function):
         return func
 
 
+
+
+
+class class_mse_loss(cost_function):
+
+    def mse_with_regularization(y_true, y_pred, weights, l1=0.0, l2=0.0):
+        mse_loss = ((y_true - y_pred) ** 2).mean()
+        l1_penalty = l1 * np.sum(np.abs(weights))
+        l2_penalty = l2 * np.sum(weights ** 2)
+        return mse_loss + l1_penalty + l2_penalty
+
+    def mse_derivative_with_regularization(y_true, y_pred, weights, l1=0.0, l2=0.0):
+        grad = 2 * (y_pred - y_true) / y_true.size
+        l1_grad = l1 * np.sign(weights)
+        l2_grad = 2 * l2 * weights
+        return grad, l1_grad + l2_grad
+
+class class_binary_cros_entropy(cost_function):
+
+
+    def binary_cross_entropy(y_true, y_pred):
+        epsilon = 1e-15  # to avoid log(0)
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+        return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+
+    def binary_cross_entropy_derivative(y_true, y_pred):
+        epsilon = 1e-15
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+        return (y_pred - y_true) / (y_pred * (1 - y_pred) * y_true.size)
+
+
+    def binary_cross_entropy_with_regularization(y_true, y_pred, weights, l1=0.0, l2=0.0):
+        base_loss = binary_cross_entropy(y_true, y_pred)
+        l1_penalty = l1 * np.sum(np.abs(weights))
+        l2_penalty = l2 * np.sum(weights ** 2)
+        return base_loss + l1_penalty + l2_penalty
+
+    def binary_cross_entropy_derivative_with_regularization(y_true, y_pred, weights, l1=0.0, l2=0.0):
+        base_grad = binary_cross_entropy_derivative(y_true, y_pred)
+        l1_grad = l1 * np.sign(weights)
+        l2_grad = 2 * l2 * weights
+        return base_grad, l1_grad + l2_grad
+
+
+class class_multiclass_cross_entropy(cost_function):
+
+    def softmax(logits):
+        exps = np.exp(logits - np.max(logits, axis=1, keepdims=True))
+        return exps / np.sum(exps, axis=1, keepdims=True)
+
+    def multiclass_cross_entropy(y_true, logits):
+        probs = softmax(logits)
+        return -np.mean(np.sum(y_true * np.log(probs + 1e-15), axis=1))
+
+    def multiclass_cross_entropy_derivative(y_true, logits):
+        probs = softmax(logits)
+        return (probs - y_true) / y_true.shape[0]
+    
+    def multiclass_cross_entropy_with_regularization(y_true, logits, weights, l1=0.0, l2=0.0):
+        base_loss = multiclass_cross_entropy(y_true, logits)
+        l1_penalty = l1 * np.sum(np.abs(weights))
+        l2_penalty = l2 * np.sum(weights ** 2)
+        return base_loss + l1_penalty + l2_penalty
+
+    def multiclass_cross_entropy_derivative_with_regularization(y_true, logits, weights, l1=0.0, l2=0.0):
+        base_grad = multiclass_cross_entropy_derivative(y_true, logits)
+        l1_grad = l1 * np.sign(weights)
+        l2_grad = 2 * l2 * weights
+        return base_grad, l1_grad + l2_grad
+
